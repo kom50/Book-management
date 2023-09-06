f74d3d63d5d28d24181db970a89a5b603e6f80fe
kom50 <omprakashkartik82280@gmail.com>
2023-09-06 20:06:53 +0530

    used process.env.PORT ('.')

--- index.ts
+++ index.ts
@@ -2,13 +2,17 @@ import express, { type Application } from 'express'
 import path from 'path'
 
 const app: Application = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/app', (req, res) => {
     res.json({
         name: 'om',
         vill: 'Mahwal'
     })
+})
+
+app.get('/test', (req, res) => {
+    res.send('<h1> Hello from server side </h1>')
 })
 
 // For production
